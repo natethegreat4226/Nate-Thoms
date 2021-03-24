8857e77ee899caa23d98bfc9892db64b6be3b783
natethegreat4226 <nthoms_4226@yahoo.com>
2021-03-24 17:40:31 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# midtermexam-part2-8jqjn9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/midtermexam-part2-8jqjn9)
---
+++ src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+  selector: "app-employee-detail",
+  templateUrl: "./employee-detail.component.html",
+  styleUrls: ["./employee-detail.component.css"]
+})
+export class EmployeeDetailComponent implements OnInit {
+  
+  
+  constructor() {}
+
+  ngOnInit() {}
+}
